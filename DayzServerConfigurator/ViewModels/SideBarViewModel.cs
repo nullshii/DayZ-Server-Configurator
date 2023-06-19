112fb8378711fafb1e5ea39cf1e2dd71f9ec6555
nullshii <14684974-nullshii@users.noreply.gitlab.com>
2023-06-19 19:33:13 +0400

    Added sidebar, Colors and TextStyles

---
+++ DayzServerConfigurator/ViewModels/SideBarViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Reactive;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace DayzServerConfigurator.ViewModels;
+
+public class SideBarViewModel : ViewModelBase
+{
+    public SideBarViewModel()
+    {
+        OpenGithubCommand = ReactiveCommand.Create(OpenGithub);
+        OpenGitlabCommand = ReactiveCommand.Create(OpenGitlab);
+    }
+
+    [Reactive] public string? Version { get; set; } = "Version 1.0.0";
+    [Reactive] public ReactiveCommand<Unit, Unit> OpenGithubCommand { get; set; }
+    [Reactive] public ReactiveCommand<Unit, Unit> OpenGitlabCommand { get; set; }
+
+    private void OpenGithub()
+    {
+    }
+    
+    private void OpenGitlab()
+    {
+    }
+}
---
+++ DayzServerConfigurator/Views/SideBar.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DayzServerConfigurator.Views;
+
+public partial class SideBar : UserControl
+{
+    public SideBar()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
