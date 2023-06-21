46b4db3831b39d0409d03248ba5f2e92bf2dcf9c
nullshii <14684974-nullshii@users.noreply.gitlab.com>
2023-06-21 15:48:19 +0400

    Added UI Updates to buttons

--- DayzServerConfigurator/ViewModels/MainWindowViewModel.cs
+++ DayzServerConfigurator/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using System.Collections.ObjectModel;
+using System.IO;
 using System.Reactive;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 
@@ -7,23 +11,32 @@ namespace DayzServerConfigurator.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    private readonly OpenFolderDialog _folderDialog;
+    private ServerConfigurator? _configurator;
+
     public MainWindowViewModel()
     {
         SelectBasePathCommand = ReactiveCommand.Create(SelectBasePath);
-        CheckModListCommand = ReactiveCommand.Create(CheckModList);
+        UpdateModListCommand = ReactiveCommand.Create(UpdateModList);
         ConfigureServerCommand = ReactiveCommand.Create(ConfigureServer);
 
+        ModList = new ObservableCollection<string>();
+        
         SideBar = new SideBarViewModel();
 
         KeysPathHolder = new PathHolderViewModel { Label = "Keys path:" };
         AddonsPathHolder = new PathHolderViewModel { Label = "Addons path:" };
         ProfilePathHolder = new PathHolderViewModel { Label = "Profile path:" };
         BattlEyePathHolder = new PathHolderViewModel { Label = "BattlEye path:" };
+
+        _folderDialog = new OpenFolderDialog { Title = "Select DayZ server directory" };
     }
 
     public ReactiveCommand<Unit, Unit> SelectBasePathCommand { get; set; }
-    public ReactiveCommand<Unit, Unit> CheckModListCommand { get; set; }
+    public ReactiveCommand<Unit, Unit> UpdateModListCommand { get; set; }
     public ReactiveCommand<Unit, Unit> ConfigureServerCommand { get; set; }
+
+    [Reactive] public ObservableCollection<string> ModList { get; set; }
 
     [Reactive] public SideBarViewModel SideBar { get; set; }
 
@@ -34,16 +47,45 @@ public class MainWindowViewModel : ViewModelBase
 
     private void ConfigureServer()
     {
-        throw new NotImplementedException();
+        if (_configurator == null)
+        {
+            // TODO: show messagebox to select server directory
+            return;
+        }
+
+        _configurator.Configure();
+    }
+    
+    private void UpdateModList()
+    {
+        if (_configurator == null)
+        {
+            // TODO: show messagebox to select server directory
+            return;
+        }
+        
+        _configurator.UpdateModList();
+        ModList.Clear();
+        
+        foreach (DirectoryInfo directory in _configurator.ModDirectories)
+        {
+            ModList.Add(directory.Name);
+        }
     }
 
-    private void CheckModList()
+    private async void SelectBasePath()
     {
-        throw new NotImplementedException();
-    }
+        if (Application.Current?.ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime app) return;
 
-    private void SelectBasePath()
-    {
-        throw new NotImplementedException();
+        string? selectedDirectory = await _folderDialog.ShowAsync(app.MainWindow);
+
+        if (selectedDirectory == null) return;
+
+        _configurator = new ServerConfigurator(new DirectoryInfo(selectedDirectory));
+        
+        KeysPathHolder.Path = _configurator.KeysDirectory.FullName;
+        AddonsPathHolder.Path = _configurator.AddonsDirectory.FullName;
+        ProfilePathHolder.Path = _configurator.ProfileDirectory.FullName;
+        BattlEyePathHolder.Path = _configurator.BattlEyeDirectory.FullName;
     }
 }
