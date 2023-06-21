e5be144f9477e7bcb8f29070fb26c986b9970d87
nullshii <14684974-nullshii@users.noreply.gitlab.com>
2023-06-21 13:32:35 +0400

    Added path holder

---
+++ DayzServerConfigurator/ViewModels/PathHolderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ReactiveUI.Fody.Helpers;
+
+namespace DayzServerConfigurator.ViewModels;
+
+public class PathHolderViewModel : ViewModelBase
+{
+    public PathHolderViewModel()
+    {
+        Label = "Label";
+        Path = "";
+    }
+
+    [Reactive] public string Label { get; set; }
+    [Reactive] public string Path { get; set; }
+}
---
+++ DayzServerConfigurator/Views/PathHolder.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DayzServerConfigurator.Views;
+
+public partial class PathHolder : UserControl
+{
+    public PathHolder()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
