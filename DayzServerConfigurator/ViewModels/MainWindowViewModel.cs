4135e2a1f6650345d7f30cbfa8040eb17410e956
nullshii <14684974-nullshii@users.noreply.gitlab.com>
2023-06-21 13:33:26 +0400

    Finished main window UI

--- DayzServerConfigurator/ViewModels/MainWindowViewModel.cs
+++ DayzServerConfigurator/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,7 @@
-﻿using ReactiveUI.Fody.Helpers;
+﻿using System;
+using System.Reactive;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace DayzServerConfigurator.ViewModels;
 
@@ -6,8 +9,41 @@ public class MainWindowViewModel : ViewModelBase
 {
     public MainWindowViewModel()
     {
+        SelectBasePathCommand = ReactiveCommand.Create(SelectBasePath);
+        CheckModListCommand = ReactiveCommand.Create(CheckModList);
+        ConfigureServerCommand = ReactiveCommand.Create(ConfigureServer);
+
         SideBar = new SideBarViewModel();
+
+        KeysPathHolder = new PathHolderViewModel { Label = "Keys path:" };
+        AddonsPathHolder = new PathHolderViewModel { Label = "Addons path:" };
+        ProfilePathHolder = new PathHolderViewModel { Label = "Profile path:" };
+        BattlEyePathHolder = new PathHolderViewModel { Label = "BattlEye path:" };
     }
 
+    public ReactiveCommand<Unit, Unit> SelectBasePathCommand { get; set; }
+    public ReactiveCommand<Unit, Unit> CheckModListCommand { get; set; }
+    public ReactiveCommand<Unit, Unit> ConfigureServerCommand { get; set; }
+
     [Reactive] public SideBarViewModel SideBar { get; set; }
+
+    [Reactive] public PathHolderViewModel KeysPathHolder { get; set; }
+    [Reactive] public PathHolderViewModel AddonsPathHolder { get; set; }
+    [Reactive] public PathHolderViewModel ProfilePathHolder { get; set; }
+    [Reactive] public PathHolderViewModel BattlEyePathHolder { get; set; }
+
+    private void ConfigureServer()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void CheckModList()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void SelectBasePath()
+    {
+        throw new NotImplementedException();
+    }
 }
